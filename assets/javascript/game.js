26f5650ed02599b0c7a6c3dce4f2f41d2f2e7448
Bryan Isles <bryanisles@gmail.com>
2017-09-27 17:54:32 -0500

    Working file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,11 +22,6 @@ console.log("The randomly selected Artist is: " + selectedArtist);
 console.log("Modified to lower case with no space as follows (str len = "+tempGuessStr.length+"): " + tempGuessStr);
 console.log("Modified to underscores (str len = "+ currentWord.length +"): "+currentWord);
 
-function setCharAt(str,index,char) {
-	if(index > str.length-1) {return str};
-	return str.substr(0,index) + char + str.substr(index+1);
-}
-
 document.onkeyup = function(event) {
 	var userGuess = event.key;
 	var tempUserGuess = userGuess.toLowerCase();
@@ -34,9 +29,10 @@ document.onkeyup = function(event) {
 	//and if the key exists as a letter in the tempGuessStr
 	if (myAlpha.indexOf(tempUserGuess) > -1 
 		&& tempGuessStr.indexOf(tempUserGuess) > -1 
-		&& lettersAlreadyGuessed.indexOf(tempUserGuess) == -1){
+		&& lettersAlreadyGuessed.indexOf(tempUserGuess) == -1) {
 		//if it is in the 26-letter alphabet and is a letter that exist in the name of the artist
 		lettersAlreadyGuessed += tempUserGuess;
+		
 		for(var j = 0; j < tempGuessStr.length; j++) {
 			if(tempUserGuess == tempGuessStr[j]) {
 				var tempCurrentWord = currentWord.split("");
@@ -45,22 +41,50 @@ document.onkeyup = function(event) {
 				document.getElementById("currentWord").innerHTML = currentWord.split("").join(" ");
 			} else {
 				//check code at this point
-				noGuess--;
-				document.getElementById("noGuesses").innerHTML = noGuess;
 			}
 		}
+		
 		if(currentWord.indexOf("_") == -1){
 			myWin++;
 			document.getElementById("currentWord").innerHTML = selectedArtist;
 			console.log(myWin);
 			noGuess = 10;
 			document.getElementById("noGuesses").innerHTML = noGuess;
+			document.getElementById("noWins").innerHTML = myWin;
+			myRnd = Math.floor(Math.random()*myArtistNames.length);
+			selectedArtist = myArtistNames[myRnd];
+			tempGuessStr = selectedArtist.replace(/\s+/g,'').toLowerCase();
+			currentWord = "";
+			
+			for (var i = 0; i < tempGuessStr.length; i++) {
+				currentWord += "_";
+			}
+		
 		} else if (noGuess == 0) {
+			
 			alert("Game Over");
 			noGuess=10;
 			document.getElementById("noGuesses").innerHTML = noGuess;
+			
+			myRnd = Math.floor(Math.random()*myArtistNames.length);
+			selectedArtist = myArtistNames[myRnd];
+			tempGuessStr = selectedArtist.replace(/\s+/g,'').toLowerCase();
+			currentWord = "";
+			
+			for (var i = 0; i < tempGuessStr.length; i++) {
+				currentWord += "_";
+			}
 		}
 
+	} else {
+		
+		if(lettersAlreadyGuessed.indexOf(tempUserGuess) == -1) {
+			noGuess--;
+			document.getElementById("noGuesses").innerHTML = noGuess;
+		}
+	
 	}
+	
+	document.getElementById("tempStr").innerHTML = lettersAlreadyGuessed;
 	//require to check if the letter was already clicked on
 }
