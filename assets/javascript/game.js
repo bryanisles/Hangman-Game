a848f854a8c21faca6bbca8c11f2fc6521668a3e
Bryan Isles <bryanisles@gmail.com>
2017-09-26 22:08:22 -0500

    Rev.A; Working Repository

---
+++ assets/javascript/game.js
@@ -0,0 +1,66 @@
+var myArtistNames = ["Logic", 
+					 "Joey Badass", 
+					 "Lecrae", 
+					 "Knowledge Above All Nonsense", 
+					 "Lupe Fiasco", 
+					 "Oddisee", 
+					 "Andy Mineo"];
+var myAlpha = "abcdefghijklmnopqrstuvwxyz";
+var lettersAlreadyGuessed = "";
+var myRnd = Math.floor(Math.random()*myArtistNames.length);
+var selectedArtist = myArtistNames[myRnd];
+var tempGuessStr = selectedArtist.replace(/\s+/g,'').toLowerCase();
+var myWin = 0;
+var noGuess = 10;
+var currentWord = "";
+for (var i = 0; i < tempGuessStr.length; i++) {
+	currentWord += "_";
+}
+
+
+console.log("The randomly selected Artist is: " + selectedArtist);
+console.log("Modified to lower case with no space as follows (str len = "+tempGuessStr.length+"): " + tempGuessStr);
+console.log("Modified to underscores (str len = "+ currentWord.length +"): "+currentWord);
+
+function setCharAt(str,index,char) {
+	if(index > str.length-1) {return str};
+	return str.substr(0,index) + char + str.substr(index+1);
+}
+
+document.onkeyup = function(event) {
+	var userGuess = event.key;
+	var tempUserGuess = userGuess.toLowerCase();
+	//check if the key pressed exists within the 26 keys in the alpha numeric
+	//and if the key exists as a letter in the tempGuessStr
+	if (myAlpha.indexOf(tempUserGuess) > -1 
+		&& tempGuessStr.indexOf(tempUserGuess) > -1 
+		&& lettersAlreadyGuessed.indexOf(tempUserGuess) == -1){
+		//if it is in the 26-letter alphabet and is a letter that exist in the name of the artist
+		lettersAlreadyGuessed += tempUserGuess;
+		for(var j = 0; j < tempGuessStr.length; j++) {
+			if(tempUserGuess == tempGuessStr[j]) {
+				var tempCurrentWord = currentWord.split("");
+				tempCurrentWord[j] = tempUserGuess;
+				currentWord = tempCurrentWord.join("");
+				document.getElementById("currentWord").innerHTML = currentWord.split("").join(" ");
+			} else {
+				//check code at this point
+				noGuess--;
+				document.getElementById("noGuesses").innerHTML = noGuess;
+			}
+		}
+		if(currentWord.indexOf("_") == -1){
+			myWin++;
+			document.getElementById("currentWord").innerHTML = selectedArtist;
+			console.log(myWin);
+			noGuess = 10;
+			document.getElementById("noGuesses").innerHTML = noGuess;
+		} else if (noGuess == 0) {
+			alert("Game Over");
+			noGuess=10;
+			document.getElementById("noGuesses").innerHTML = noGuess;
+		}
+
+	}
+	//require to check if the letter was already clicked on
+}
